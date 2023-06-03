bc6eba2bdf87fee042650b82be021399ae69a88e
giammy677dev <giammy677@gmail.com>
2023-06-03 10:19:37 +0200

    Gitignore

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ def print_hi(name):
 if __name__ == '__main__':
     print_hi('PyCharm')
 
-print("GM")
+print("GM2")
 print("CIAOOOOOO")
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
 print("aas")
