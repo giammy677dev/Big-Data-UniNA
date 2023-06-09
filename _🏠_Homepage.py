556205b8ce33a31c9afae19b68e0bb564945ded0
Mav3rick26 <marcoperillo26@gmail.com>
2023-06-09 13:02:19 +0200

    Prima impostazione della Home Page

--- _🏠_Homepage.py
+++ _🏠_Homepage.py
@@ -1,26 +1,31 @@
 from utils import st
 
 st.set_page_config(
-    page_title="Hello",
-    page_icon="👋",
+    page_title="TwitterAnalytics",
+    page_icon="📊",
 )
 
-st.write("# Welcome to Streamlit! 👋")
+st.image("copertinaHomePage.jpg")
 
-st.markdown(
-    """
-    Streamlit is an open-source app framework built specifically for
-    Machine Learning and Data Science projects.
-    **👈 Select a demo from the sidebar** to see some examples
-    of what Streamlit can do!
-    ### Want to learn more?
-    - Check out [streamlit.io](https://streamlit.io)
-    - Jump into our [documentation](https://docs.streamlit.io)
-    - Ask a question in our [community
-        forums](https://discuss.streamlit.io)
-    ### See more complex demos
-    - Use a neural net to [analyze the Udacity Self-driving Car Image
-        Dataset](https://github.com/streamlit/demo-self-driving)
-    - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
-"""
-)
+st.title("Dashboard Analitiche Social Network")
+st.header("Analitiche e summarization - Twitter")
+st.write("""
+  Inserire piccola presentazione della dashboard
+  """)
+
+st.write("""
+      Le analitiche effettuate che ritroviamo in questa dashboard sono le seguenti:
+      """)
+
+
+# Elenco con bullet list e link ai collegamenti delle sezioni
+def elenco_bullet(testo_grassetto, testo_normale):
+    st.markdown(f"- <span style='color:#00acee'><b>{testo_grassetto}</b></span>: {testo_normale}",
+                unsafe_allow_html=True)
+
+
+# Esempio di utilizzo
+elenco_bullet("Prima sezione di Analitiche", "Questo settore delle analitiche è user-oriented in quanto si concentra sull'analisi del comportamento di uno specifico utente selezionabile e sulle interazioni effettuate all'interno della piattaforma. Attraverso operazioni di summarization e di svolte sui dati estratti dai tweet, l'obiettivo principale è comprendere le opinioni e i sentiment degli utenti.")
+elenco_bullet("Seconda sezione di Analitiche", "Questo settore delle analitiche è topic-oriented, infatti, le analisi si concentrano sui dati provenienti dai tweet relativi a specifici argomenti di interesse trattati, identificati considerando l'intera totalità di tweet.")
+elenco_bullet("Terza sezione di Analitiche", "Questo settore si concentra principalmente nell'identificazione dei cosiddetti influencer, e tutte le analisi fanno riferimento all'infuencer selezionato.")
+
